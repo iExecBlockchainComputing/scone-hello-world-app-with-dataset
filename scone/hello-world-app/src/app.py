1e19208d652d54e05247859137439031d8c82899
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-06 10:33:36 +0100

    Rename result file

--- scone/hello-world-app/src/app.py
+++ scone/hello-world-app/src/app.py
@@ -1,6 +1,6 @@
 # print to stdout
 print("Hello from inside the enclave!")
 
-# produce a result file
-with open("/scone/my-result-file.txt", "w+") as result_file:
+# produce a result file in /scone
+with open("/scone/my-result.txt", "w+") as result_file:
     result_file.write("It's dark over here!")
