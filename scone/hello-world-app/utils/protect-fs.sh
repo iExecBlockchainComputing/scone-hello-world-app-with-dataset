3be23d4cdebd61078b5ce5f9a1d9cb3ae6e7dcc9
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-11 09:54:00 +0100

    Take snaphsot (:white_check_mark:)

--- scone/hello-world-app/utils/protect-fs.sh
+++ scone/hello-world-app/utils/protect-fs.sh
@@ -1,16 +1,12 @@
 # #!/bin/bash
 
-# # export SCONE_HASH=1
 export SCONE_MODE=sim
 export SCONE_HEAP=1G
 export SCONE_ALPINE=1
 
-# APP_FOLDER=$1
-
-# printf "\n### Starting file system protection ...\n\n"
+###############################
 
 # scone fspf create /tmp/fspf.pb
-
 # scone fspf addr /tmp/fspf.pb /          --not-protected --kernel /
 # scone fspf addr /tmp/fspf.pb /usr       --authenticated --kernel /usr
 # scone fspf addf /tmp/fspf.pb /usr       /usr
@@ -31,21 +27,11 @@ export SCONE_ALPINE=1
 
 # scone fspf encrypt /tmp/fspf.pb > /tmp/keytag
 
-# MRENCLAVE="$(SCONE_HASH=1 python)"
-# FSPF_TAG=$(cat /tmp/keytag | awk '{print $9}')
-# FSPF_KEY=$(cat /tmp/keytag | awk '{print $11}')
-# FINGERPRINT="$FSPF_KEY|$FSPF_TAG|$MRENCLAVE"
-
-
-# printf "\n\n"
-# printf "#####################################################################\n"
-# printf "Application's fingerprint (use this when deploying your app onchain):\n"
-# printf "$FINGERPRINT\n"
-# printf "#####################################################################\n"
-# printf "\n\n"
-
-
 ###############################
+
+APP_FOLDER=$1
+
+printf "\n### Starting file system protection ...\n\n"
 
 mkdir conf							    \
 	&& scone fspf create fspf.pb 					    \
@@ -56,18 +42,30 @@ mkdir conf							    \
 	&& scone fspf addf fspf.pb /bin /bin 			            \
 	&& scone fspf addr fspf.pb /lib --authenticated --kernel /lib       \
 	&& scone fspf addf fspf.pb /lib /lib 			            \
-	&& scone fspf addr fspf.pb /etc --authenticated --kernel /etc       \
-	&& scone fspf addf fspf.pb /etc /etc 			            \
+	&& scone fspf addr fspf.pb /etc/ssl --authenticated --kernel /etc/ssl       \
+	&& scone fspf addf fspf.pb /etc/ssl /etc/ssl 			            \
 	&& scone fspf addr fspf.pb /sbin --authenticated --kernel /sbin     \
 	&& scone fspf addf fspf.pb /sbin /sbin 			            \
 	&& scone fspf addr fspf.pb /signer --authenticated --kernel /signer \
 	&& scone fspf addf fspf.pb /signer /signer 			    \
-	&& scone fspf addr fspf.pb /app --authenticated --kernel /app 	    \
-	&& scone fspf addf fspf.pb /app /app 				    \
-	&& scone fspf encrypt ./fspf.pb > /conf/keytag 			    \
-	&& MRENCLAVE="$(SCONE_HASH=1 python)"			            \
-	&& FSPF_TAG=$(cat conf/keytag | awk '{print $9}') 	            \
-	&& FSPF_KEY=$(cat conf/keytag | awk '{print $11}')		    \
-	&& FINGERPRINT="$FSPF_KEY|$FSPF_TAG|$MRENCLAVE"			    \
-	&& echo $FINGERPRINT > conf/fingerprint.txt			    \
-	&& printf "\n########################################################\nMREnclave: $FINGERPRINT\n########################################################\n\n"
+	&& scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER 	    \
+	&& scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER 				    \
+	&& scone fspf encrypt ./fspf.pb > /conf/keytag
+	# && MRENCLAVE="$(SCONE_HASH=1 python)"			            \
+	# && FSPF_TAG=$(cat conf/keytag | awk '{print $9}') 	            \
+	# && FSPF_KEY=$(cat conf/keytag | awk '{print $11}')		    \
+	# && FINGERPRINT="$FSPF_KEY|$FSPF_TAG|$MRENCLAVE"			    \
+	# && echo $FINGERPRINT > conf/fingerprint.txt			    \
+	# && printf "\n########################################################\nMREnclave: $FINGERPRINT\n########################################################\n\n"
+
+MRENCLAVE="$(SCONE_HASH=1 python)"
+FSPF_TAG=$(cat conf/keytag | awk '{print $9}')
+FSPF_KEY=$(cat conf/keytag | awk '{print $11}')
+FINGERPRINT="$FSPF_KEY|$FSPF_TAG|$MRENCLAVE"
+
+printf "\n\n"
+printf "#####################################################################\n"
+printf "Application's fingerprint (use this when deploying your app onchain):\n"
+printf "$FINGERPRINT\n"
+printf "#####################################################################\n"
+printf "\n\n"
