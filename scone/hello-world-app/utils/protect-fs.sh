1b447c97b3ce5978922a5d881231fc073ca761ef
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-11 12:28:26 +0100

    Take snaphsot (:white_check_mark:)

--- scone/hello-world-app/utils/protect-fs.sh
+++ scone/hello-world-app/utils/protect-fs.sh
@@ -8,26 +8,26 @@ APP_FOLDER=$1
 
 printf "\n### Starting file system protection ...\n\n"
 
-scone fspf create fspf.pb
-scone fspf addr fspf.pb /          --not-protected --kernel /
-scone fspf addr fspf.pb /usr       --authenticated --kernel /usr
-scone fspf addf fspf.pb /usr       /usr
-scone fspf addr fspf.pb /bin       --authenticated --kernel /bin
-scone fspf addf fspf.pb /bin       /bin
-scone fspf addr fspf.pb /lib       --authenticated --kernel /lib
-scone fspf addf fspf.pb /lib       /lib
-scone fspf addr fspf.pb /etc/ssl   --authenticated --kernel /etc/ssl
-scone fspf addf fspf.pb /etc/ssl   /etc/ssl
-scone fspf addr fspf.pb /sbin      --authenticated --kernel /sbin
-scone fspf addf fspf.pb /sbin      /sbin
-scone fspf addr fspf.pb /signer    --authenticated --kernel /signer
-scone fspf addf fspf.pb /signer    /signer
+scone fspf create /fspf.pb
+scone fspf addr /fspf.pb /          --not-protected --kernel /
+scone fspf addr /fspf.pb /usr       --authenticated --kernel /usr
+scone fspf addf /fspf.pb /usr       /usr
+scone fspf addr /fspf.pb /bin       --authenticated --kernel /bin
+scone fspf addf /fspf.pb /bin       /bin
+scone fspf addr /fspf.pb /lib       --authenticated --kernel /lib
+scone fspf addf /fspf.pb /lib       /lib
+scone fspf addr /fspf.pb /etc/ssl   --authenticated --kernel /etc/ssl
+scone fspf addf /fspf.pb /etc/ssl   /etc/ssl
+scone fspf addr /fspf.pb /sbin      --authenticated --kernel /sbin
+scone fspf addf /fspf.pb /sbin      /sbin
+scone fspf addr /fspf.pb /signer    --authenticated --kernel /signer
+scone fspf addf /fspf.pb /signer    /signer
 
 printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"
-scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER
-scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER
+scone fspf addr /fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER
+scone fspf addf /fspf.pb $APP_FOLDER $APP_FOLDER
 
-scone fspf encrypt fspf.pb > /tmp/keytag
+scone fspf encrypt /fspf.pb > /tmp/keytag
 
 MRENCLAVE="$(SCONE_HASH=1 python)"
 FSPF_TAG=$(cat /tmp/keytag | awk '{print $9}')
