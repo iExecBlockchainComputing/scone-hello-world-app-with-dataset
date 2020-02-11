d31be7a86e779fdba1dda75dd098791dddf01322
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-11 11:15:04 +0100

    Take snaphsot (:white_check_mark:)

--- scone/hello-world-app/utils/protect-fs.sh
+++ scone/hello-world-app/utils/protect-fs.sh
@@ -10,51 +10,51 @@ printf "\n### Starting file system protection ...\n\n"
 
 ###############################
 
-# mkdir conf
+mkdir conf
 
-# scone fspf create /tmp/fspf.pb
-# scone fspf addr /tmp/fspf.pb /          --not-protected --kernel /
-# scone fspf addr /tmp/fspf.pb /usr       --authenticated --kernel /usr
-# scone fspf addf /tmp/fspf.pb /usr       /usr
-# scone fspf addr /tmp/fspf.pb /bin       --authenticated --kernel /bin
-# scone fspf addf /tmp/fspf.pb /bin       /bin
-# scone fspf addr /tmp/fspf.pb /lib       --authenticated --kernel /lib
-# scone fspf addf /tmp/fspf.pb /lib       /lib
-# scone fspf addr /tmp/fspf.pb /etc/ssl   --authenticated --kernel /etc/ssl
-# scone fspf addf /tmp/fspf.pb /etc/ssl   /etc/ssl
-# scone fspf addr /tmp/fspf.pb /sbin      --authenticated --kernel /sbin
-# scone fspf addf /tmp/fspf.pb /sbin      /sbin
-# scone fspf addr /tmp/fspf.pb /signer    --authenticated --kernel /signer
-# scone fspf addf /tmp/fspf.pb /signer    /signer
+scone fspf create fspf.pb
+scone fspf addr fspf.pb /          --not-protected --kernel /
+scone fspf addr fspf.pb /usr       --authenticated --kernel /usr
+scone fspf addf fspf.pb /usr       /usr
+scone fspf addr fspf.pb /bin       --authenticated --kernel /bin
+scone fspf addf fspf.pb /bin       /bin
+scone fspf addr fspf.pb /lib       --authenticated --kernel /lib
+scone fspf addf fspf.pb /lib       /lib
+scone fspf addr fspf.pb /etc/ssl   --authenticated --kernel /etc/ssl
+scone fspf addf fspf.pb /etc/ssl   /etc/ssl
+scone fspf addr fspf.pb /sbin      --authenticated --kernel /sbin
+scone fspf addf fspf.pb /sbin      /sbin
+scone fspf addr fspf.pb /signer    --authenticated --kernel /signer
+scone fspf addf fspf.pb /signer    /signer
 
-# printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"
-# scone fspf addr /tmp/fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER
-# scone fspf addf /tmp/fspf.pb $APP_FOLDER $APP_FOLDER
+printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"
+scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER
+scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER
 
-# scone fspf encrypt /tmp/fspf.pb > /tmp/keytag
+scone fspf encrypt fspf.pb > conf/keytag
 
 ###############################
 
-mkdir conf
+# mkdir conf
 
-scone fspf create fspf.pb && \
-	scone fspf addr fspf.pb /           --not-protected --kernel /          && \
-	scone fspf addr fspf.pb /usr        --authenticated --kernel /usr       && \
-	scone fspf addf fspf.pb /usr        /usr 			                    && \
-	scone fspf addr fspf.pb /bin        --authenticated --kernel /bin       && \
-	scone fspf addf fspf.pb /bin        /bin 			                    && \
-	scone fspf addr fspf.pb /lib        --authenticated --kernel /lib       && \
-	scone fspf addf fspf.pb /lib        /lib 			                    && \
-	scone fspf addr fspf.pb /etc/ssl    --authenticated --kernel /etc/ssl   && \
-	scone fspf addf fspf.pb /etc/ssl    /etc/ssl 			                && \
-	scone fspf addr fspf.pb /sbin       --authenticated --kernel /sbin      && \
-	scone fspf addf fspf.pb /sbin       /sbin 			                    && \
-	scone fspf addr fspf.pb /signer     --authenticated --kernel /signer    && \
-	scone fspf addf fspf.pb /signer     /signer 			                && \
-    printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"      && \
-	scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER && \
-	scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER 				        && \
-	scone fspf encrypt ./fspf.pb > /conf/keytag
+# scone fspf create fspf.pb && \
+#     scone fspf addr fspf.pb /           --not-protected --kernel /          && \
+#     scone fspf addr fspf.pb /usr        --authenticated --kernel /usr       && \
+#     scone fspf addf fspf.pb /usr        /usr 			                    && \
+#     scone fspf addr fspf.pb /bin        --authenticated --kernel /bin       && \
+#     scone fspf addf fspf.pb /bin        /bin 			                    && \
+#     scone fspf addr fspf.pb /lib        --authenticated --kernel /lib       && \
+#     scone fspf addf fspf.pb /lib        /lib 			                    && \
+#     scone fspf addr fspf.pb /etc/ssl    --authenticated --kernel /etc/ssl   && \
+#     scone fspf addf fspf.pb /etc/ssl    /etc/ssl 			                && \
+#     scone fspf addr fspf.pb /sbin       --authenticated --kernel /sbin      && \
+#     scone fspf addf fspf.pb /sbin       /sbin 			                    && \
+#     scone fspf addr fspf.pb /signer     --authenticated --kernel /signer    && \
+#     scone fspf addf fspf.pb /signer     /signer 			                && \
+#     printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"      && \
+#     scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER && \
+#     scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER 				        && \
+#     scone fspf encrypt ./fspf.pb > /conf/keytag
 
 ###############################
 
