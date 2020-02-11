93f9f4967e0ed090f45cac17c2118518941a9eb9
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-11 10:47:51 +0100

    Take snaphsot (:white_check_mark:)

--- scone/hello-world-app/utils/protect-fs.sh
+++ scone/hello-world-app/utils/protect-fs.sh
@@ -4,7 +4,13 @@ export SCONE_MODE=sim
 export SCONE_HEAP=1G
 export SCONE_ALPINE=1
 
+APP_FOLDER=$1
+
+printf "\n### Starting file system protection ...\n\n"
+
 ###############################
+
+# mkdir conf
 
 # scone fspf create /tmp/fspf.pb
 # scone fspf addr /tmp/fspf.pb /          --not-protected --kernel /
@@ -29,34 +35,28 @@ export SCONE_ALPINE=1
 
 ###############################
 
-APP_FOLDER=$1
+mkdir conf
 
-printf "\n### Starting file system protection ...\n\n"
+scone fspf create fspf.pb && \
+	scone fspf addr fspf.pb /           --not-protected --kernel /          && \
+	scone fspf addr fspf.pb /usr        --authenticated --kernel /usr       && \
+	scone fspf addf fspf.pb /usr        /usr 			                    && \
+	scone fspf addr fspf.pb /bin        --authenticated --kernel /bin       && \
+	scone fspf addf fspf.pb /bin        /bin 			                    && \
+	scone fspf addr fspf.pb /lib        --authenticated --kernel /lib       && \
+	scone fspf addf fspf.pb /lib        /lib 			                    && \
+	scone fspf addr fspf.pb /etc/ssl    --authenticated --kernel /etc/ssl   && \
+	scone fspf addf fspf.pb /etc/ssl    /etc/ssl 			                && \
+	scone fspf addr fspf.pb /sbin       --authenticated --kernel /sbin      && \
+	scone fspf addf fspf.pb /sbin       /sbin 			                    && \
+	scone fspf addr fspf.pb /signer     --authenticated --kernel /signer    && \
+	scone fspf addf fspf.pb /signer     /signer 			                && \
+    printf "\n### Protecting code found in folder \"$APP_FOLDER\"\n\n"      && \
+	scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER && \
+	scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER 				        && \
+	scone fspf encrypt ./fspf.pb > /conf/keytag
 
-mkdir conf							    \
-	&& scone fspf create fspf.pb 					    \
-	&& scone fspf addr fspf.pb /  --not-protected --kernel /            \
-	&& scone fspf addr fspf.pb /usr --authenticated --kernel /usr       \
-	&& scone fspf addf fspf.pb /usr /usr 			            \
-	&& scone fspf addr fspf.pb /bin --authenticated --kernel /bin       \
-	&& scone fspf addf fspf.pb /bin /bin 			            \
-	&& scone fspf addr fspf.pb /lib --authenticated --kernel /lib       \
-	&& scone fspf addf fspf.pb /lib /lib 			            \
-	&& scone fspf addr fspf.pb /etc/ssl --authenticated --kernel /etc/ssl       \
-	&& scone fspf addf fspf.pb /etc/ssl /etc/ssl 			            \
-	&& scone fspf addr fspf.pb /sbin --authenticated --kernel /sbin     \
-	&& scone fspf addf fspf.pb /sbin /sbin 			            \
-	&& scone fspf addr fspf.pb /signer --authenticated --kernel /signer \
-	&& scone fspf addf fspf.pb /signer /signer 			    \
-	&& scone fspf addr fspf.pb $APP_FOLDER --authenticated --kernel $APP_FOLDER 	    \
-	&& scone fspf addf fspf.pb $APP_FOLDER $APP_FOLDER 				    \
-	&& scone fspf encrypt ./fspf.pb > /conf/keytag
-	# && MRENCLAVE="$(SCONE_HASH=1 python)"			            \
-	# && FSPF_TAG=$(cat conf/keytag | awk '{print $9}') 	            \
-	# && FSPF_KEY=$(cat conf/keytag | awk '{print $11}')		    \
-	# && FINGERPRINT="$FSPF_KEY|$FSPF_TAG|$MRENCLAVE"			    \
-	# && echo $FINGERPRINT > conf/fingerprint.txt			    \
-	# && printf "\n########################################################\nMREnclave: $FINGERPRINT\n########################################################\n\n"
+###############################
 
 MRENCLAVE="$(SCONE_HASH=1 python)"
 FSPF_TAG=$(cat conf/keytag | awk '{print $9}')
