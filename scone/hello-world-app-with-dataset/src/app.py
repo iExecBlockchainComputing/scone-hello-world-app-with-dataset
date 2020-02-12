69600fa7f9220ac9bd89ceb72ff62cef31d97e4d
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-12 14:10:50 +0100

    Take snaphsot

--- scone/hello-world-app-with-dataset/src/app.py
+++ scone/hello-world-app-with-dataset/src/app.py
@@ -1,5 +1,19 @@
-with open("/iexec_in/dataset.txt", "r") as fin:
-   with open("/scone/result.txt", "w+") as fout:
-       data = fin.read()
-       fout.write(data)
-       print(data)
+
+# open dataset file
+with open("/iexec_in/hello-world.txt", "r") as dataset_file:
+    data = dataset_file.read()
+
+    # print dataset content to stdout
+    print("Dataset content: " + data)
+
+    # open result file and write dataset file content
+    with open("/scone/my-result.txt", "w+") as result_file:
+        result_file.write(data)
+
+
+# print to stdout
+print("Hello from inside the enclave!")
+
+# write the content of the dataset to a result file
+with open("/scone/my-result-file.txt", "w+") as result_file:
+    result_file.write("It's dark over here!")
