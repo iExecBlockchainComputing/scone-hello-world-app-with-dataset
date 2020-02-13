9ca6f8ff9cd6e8e566e3fffadc81d50a86be85eb
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-13 17:11:33 +0100

    Change input file path

--- src/app.py
+++ src/app.py
@@ -8,5 +8,5 @@ def write_file(path, data):
         f.write(data)
 
 if __name__ == "__main__":
-    data = read_file("/iexec_in/my-first-dataset/hello-world.txt")
+    data = read_file("/iexec_in/hello-world.txt")
     write_file("/scone/my-result.txt", data)
