2e4546c09282764ded44cc7a673291d7b20aaf97
zguesmi <26070035+zguesmi@users.noreply.github.com>
2020-02-13 14:26:40 +0100

    Clean

--- scone/hello-world-app-with-dataset/src/app.py
+++ scone/hello-world-app-with-dataset/src/app.py
@@ -1,12 +1,3 @@
-import os
-import zipfile
-
-
-# def unzip_dataset():
-#     dataset_filepath = "/iexec_in/" + os.environ["IEXEC_DATASET_FILENAME"]
-#     print("==> dataset: " + dataset_filepath)
-#     with zipfile.ZipFile(dataset_filepath, 'r') as zip:
-#         zip.extractall("/iexec_in")
 
 def read_file(filepath):
     with open(filepath, "r") as f:
@@ -16,13 +7,6 @@ def write_file(path, data):
     with open(path, "w+") as f:
         f.write(data)
 
-
 if __name__ == "__main__":
-    input_filepath = "/iexec_in/my-first-dataset/hello-world.txt"
-    result_filepath = "/scone/my-result.txt"
-
-    # unzip_dataset()
-    print("### content of /iexec_in: " + os.listdir("/iexec_in"))
-
-    data = read_file(input_filepath)
-    write_file(result_filepath, data)
+    data = read_file("/iexec_in/my-first-dataset/hello-world.txt")
+    write_file("/scone/my-result.txt", data)
